6ea2211cdd7651aea6ebfc279da740b3dc6c1894
llvll0hsen <mohsen.jadidi@gmail.com>
2017-09-13 12:31:17 +0200

    remove google_account

--- google_account.py
+++
@@ -1,9 +0,0 @@
-api_key = 'AIzaSyCGJECJ4aZ97HLgS4alKzFhovbKASyQsqE'
-client_id = '542762914818-8uq95q7mnu3a07pnphsh7g88ko96v66u.apps.googleusercontent.com'
-client_secret = 'wO8AICMCUDypLTmKmeZ7TLI9'
-
-account = {'mohsen':
-            {"api_key": api_key,
-            "clinet_id": client_id,
-            "client_secret": client_secret}
-            }
