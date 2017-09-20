d908526343d331fe9942c7264fd5c863159b5ade
llvll0hsen <mohsen.jadidi@gmail.com>
2017-09-20 12:50:04 +0200

    add util

---
+++ util.py
@@ -0,0 +1,6 @@
+import os
+
+output_path = os.path.join(os.getcwd(),'output')
+output_path_files = os.path.join(os.getcwd(),'output','files')
+output_path_plots = os.path.join(os.getcwd(),'output','plots')
+
